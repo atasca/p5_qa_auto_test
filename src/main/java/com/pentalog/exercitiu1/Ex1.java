d4a7fb2b345157e69c07d5114cb40fc583088f57
bogdanleustean <bogdan.leustean@yahoo.com>
2021-08-19 18:54:23 +0300

    rezolvareEx1

--- src/main/java/com/pentalog/exercitiu1/Ex1.java
+++ src/main/java/com/pentalog/exercitiu1/Ex1.java
@@ -1,6 +1,11 @@
 package com.pentalog.exercitiu1;
 
 
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 
 
@@ -16,19 +21,62 @@ public class Ex1 {
   static String textDeCitit = "He has a car, but the car it is old but he should sell it.";
 
 
-  public static void main (String[] args) {
+  public static void main(String[] args) {
 
     verificaCuvantul(returneazaPrimulCuvantDuplicat());
   }
 
-
-  static String returneazaPrimulCuvantDuplicat () {
+  static String returneazaPrimulCuvantDuplicat() {
     //rezolvare
-    return "rezultat";
-  }
+ /*
+    // variabila pentru numararea cuvintelor
+    int count;
+    // string to lowerCase
+    textDeCitit = textDeCitit.toLowerCase();
+    // impartim stringul in cuvinte
+    String cuvinte[] = textDeCitit.split(" ");
+    for (int i = 0; i < cuvinte.length; i++) {
+      count = 1;
+      for (int j = i + 1; j < cuvinte.length; j++) {
+        if (cuvinte[i].equals(cuvinte[j])) {
+          count++;
+          cuvinte[j] = "0";
+        }
+      }
+      if (count > 1 && cuvinte[i] != "0") {
+        System.out.println(cuvinte[i]);
+      }
+    }
+  */
+    // am incercat  dar mi-a dat cu virgula,   cred ca trebuie sa-ti adaptezi codul la Business requirements
+    InputStream fraza = Ex1.class.getClassLoader().getResourceAsStream("Exercitiu1.txt");
 
+    String[] words = textDeCitit.split("\\W");
+
+    Map<String, Integer> cuvinteMapate = new HashMap<>();
+
+    for (String word : words) {
+
+      if (cuvinteMapate.get(word) != null) {
+        cuvinteMapate.put(word, cuvinteMapate.get(word) + 1);
+      }
+      else {
+        cuvinteMapate.put(word, 1);
+      }
+    }
+    Set<String> cuvinteSet = cuvinteMapate.keySet();
+
+    for (String word : cuvinteSet) {
+
+      if (cuvinteMapate.get(word) > 1)
+
+        System.out.println(word);
+    }
+
+      return "rezultat";
+    }
 
   public static void verificaCuvantul (String rezultat) {
-    assertEquals("xxxx", rezultat);
+    assertEquals(returneazaPrimulCuvantDuplicat(), rezultat);
   }
 }
